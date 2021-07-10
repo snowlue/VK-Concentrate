c0b9dabda17501f4aa12dccd40f5acc145ecc896
Pavel Ovchinnikov <opavel@internet.ru>
2021-07-10 20:37:30 +0300

    Обновил версию

--- vk-concentrate.user.js
+++ vk-concentrate.user.js
@@ -2,7 +2,7 @@
 // @name         VK Concentrate
 // @namespace    http://tampermonkey.net/
 // @description  Нажмите Alt+A на любой странице ВКонтакте и сконцентрируйтесь на важном
-// @version      1.1
+// @version      1.2
 // @author       PaveTranquil
 // @match        https://vk.com/*
 // @copyright    2021, PaveTranquil (https://vk.com/pavetranquil)
